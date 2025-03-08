7667b1877e95381178abf96656ad932d7102b41f
Mikolaj Franciszek Jakowski <mikojakowski@gmail.com>
2025-03-08 17:30:31 -0500

    updated

--- FCNN/FCNN_Base_Optim.py
+++ FCNN/FCNN_Base_Optim.py
@@ -205,8 +205,8 @@ def objective(trial):
 
 
 if __name__ == "__main__":
-    study = optuna.create_study(direction="minimize")
-    study.optimize(objective, n_trials=100, timeout=600)
+    study = optuna.create_study(direction="maximize")
+    study.optimize(objective, n_trials=100)
 
     pruned_trials = study.get_trials(deepcopy=False, states=[TrialState.PRUNED])
     complete_trials = study.get_trials(deepcopy=False, states=[TrialState.COMPLETE])

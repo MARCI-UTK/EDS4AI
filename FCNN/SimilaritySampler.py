6dcbc85d1b90cc2653889eafc74a1ac85e5855a0
Mikolaj Jakowski <mikojakowski@gmail.com>
2025-03-08 21:42:55 -0500

    add option for random shuffling to Similarity Sampler class

--- FCNN/SimilaritySampler.py
+++ FCNN/SimilaritySampler.py
@@ -3,15 +3,17 @@ import torch.utils.data
 import numpy as np
 
 class SimilaritySampler(torch.utils.data.Sampler):
-    def __init__(self, subset_percentage, quantiles, duration, type='similarity'):
+    def __init__(self, subset_percentage, quantiles, duration, type='similarity', shuffle=True):
         self.quantiles = quantiles
         self.current_epoch = 0
         self.duration = duration
+        self.type = type
+        self.shuffle = shuffle
         self.all_indices = np.arange(0, quantiles.shape[0])
 
-        if type == 'similarity' :
+        if self.type == 'similarity' :
             mask = quantiles <= subset_percentage
-        elif type == 'disimilarity':
+        elif self.type == 'disimilarity':
             mask = quantiles >= 1 - subset_percentage
 
         self.subset = self.all_indices[mask]
@@ -22,15 +24,20 @@ class SimilaritySampler(torch.utils.data.Sampler):
 
     def __iter__(self):
         if self.current_epoch < self.duration :
-            return iter(self.subset)
-
+            indices = self.subset
         else :
-            return iter(self.all_indices)
+            indices = self.all_indices
 
+        if self.shuffle :
+            indices = indices[torch.randperm(indices.shape[0])]
+        
+
+        return iter(indices)
 
 
 from torch.utils.data import TensorDataset, DataLoader
 if __name__ == "__main__" :
+    torch.manual_seed(0)
     data = torch.from_numpy(np.array([30, 40, 50, 60, 70]))
     labels = torch.from_numpy(np.array([1, 0, 0, 1, 1]))
     quantiles = torch.from_numpy(np.array([0.3, 0.8, 0.8, 0.3, 0.3]))
